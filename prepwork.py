94ae4ae4df40d1e0cd458e1d7601a698b6a52692
mandellarey-cell <mandellarey@gmail.com>
2026-07-05 21:58:44 -0400

    Implement name and age verification script
    Add a simple interactive script to ask for name and age.

---
+++ prepwork.py
@@ -0,0 +1,13 @@
+myname = "mandella"
+myage = 26
+nameques = "what is your name ?"
+print("what is your name ?")
+if input(nameques) == myname:
+  print("welcome boss")
+  else
+  print("wrong name playa")
+print("what age are you ?")
+if input("what age are you ?") !== myage:
+  print("nah wrong age mate")
+  else :
+  print("looks good on you boss")
