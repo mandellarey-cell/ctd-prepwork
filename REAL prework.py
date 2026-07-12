f3e44acdb080b2f25eff553c3569ea2b7871a9c3
mandellarey-cell <mandellarey@gmail.com>
2026-07-12 16:19:23 -0400

    Add interactive name and future guessing game

---
+++ REAL prework.py
@@ -0,0 +1,18 @@
+myname = "mandella"
+myfuture = "bright"
+
+print("what's my name ?")
+user_name = input()
+
+if user_name == myname:
+    print("nice")
+else:
+    print("wrong name playa")
+
+print("what's my future ?")
+user_response = input()
+
+if user_response == myfuture:
+    print("ifyk")
+else:
+    print("guess again")
